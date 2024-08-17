5d9e83a0571cbc3d2bf098a53ea6a96dfdcfdca3
MNSCORZA <nahusco@gmail.com>
2024-08-17 17:35:28 -0300

    se hicieron modificaciones

---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+
+const volverArriba = document.querySelector(".volver");
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+volverArriba.addEventListener("click", scrollToTop);
